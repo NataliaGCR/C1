d588c9def42d5750cff0ab090c334bbe436ce22b
NataliaGCR <152062505+NataliaGCR@users.noreply.github.com>
2024-12-22 18:15:44 +0100

    Update TP2_5.m

--- TP2_5.m
+++ TP2_5.m
@@ -7,9 +7,9 @@ gray_frame1 = rgb2gray(frame1);
 frames_of_interest = [150, 300, 450, 600];
 num_subplots = length(frames_of_interest);
 
-figure; % Crear una sola figura para los subplots
+figure;
 
-subplot_index = 1; % Índice para controlar los subplots
+subplot_index = 1;
 for k = 2:num_frames
     frame2 = read(video, k);
     gray_frame2 = rgb2gray(frame2);
@@ -18,7 +18,6 @@ for k = 2:num_frames
     if ismember(k, frames_of_interest)
         img = flowToColor(u, v);
         
-        % Crear un subplot para este fotograma de interés
         subplot(2, 2, subplot_index); % 2x2 subplots
         imshow(img);
         title([num2str(k)]);
@@ -29,6 +28,5 @@ for k = 2:num_frames
     gray_frame1 = gray_frame2;
 end
 
-% Agregar un título grande a la figura
 sgtitle('Alpha = 0.6 & # iterations = 150');
 
